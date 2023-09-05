f8e3543f60a1a44dbf21b0d496b26d7d0f8c19e0
Raja Haseeb <rajahaseeb147@gmail.com>
2023-09-05 09:50:56 +0900

    Create gdc.py

---
+++ gdc.py
@@ -0,0 +1,26 @@
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+class LinearBlock(nn.Module):
+    def __init__(self, in_c, out_c, kernel=(1, 1), stride=(1, 1), padding=(0, 0), groups=1):
+        super(LinearBlock, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_c, out_c, kernel, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(num_features=out_c)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+class GDC(nn.Module):
+    def __init__(self, embedding_size):
+        super(GDC, self).__init__()
+        self.layers = nn.Sequential(
+            LinearBlock(512, 512, groups=512, kernel=(7, 7), stride=(1, 1), padding=(0, 0)),
+            Flatten(),
+            nn.Linear(512, embedding_size, bias=False),
+            nn.BatchNorm1d(embedding_size))
+
+    def forward(self, x):
+        return self.layers(x)
